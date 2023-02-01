cbe73a2d2a4a310ecf68efd06f107158a283a527
Sondre.Bolland1 <sondre.bolland@student.uib.no>
2023-02-01 15:16:22 +0100

    Skrivefeil

--- src/main/java/no/uib/inf101/terminal/DummyShell.java
+++ src/main/java/no/uib/inf101/terminal/DummyShell.java
@@ -3,7 +3,7 @@ package no.uib.inf101.terminal;
 // UiB INF101 ShellLab - DynamicShell.java
 //
 // Denne koden er gitt som et eksempel på en klasse som implementerer
-// CommanLineInterface. Du trenger ikke gjøre noen endringer i denne
+// CommandLineInterface. Du trenger ikke gjøre noen endringer i denne
 // filen for denne lab'en (med mindre du selv ønsker).
 
 public class DummyShell implements CommandLineInterface {
